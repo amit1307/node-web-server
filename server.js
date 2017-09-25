d4aa6fd5366945e70789cd37fe6a5f54e7f8b5a7
garga9 <garga9@bp.com>
2017-09-25 16:37:53 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+'/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  fs.appendFile('server.log', `\n${now}: ${req.method} ${req.url}`, (err) => {
+    if (err) {
+      console.log('Unable to log the request');
+    }
+  });
+  console.log(`${now}: ${req.method} ${req.url}`);
+  next();
+})
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+// app.get('/', (req, res) => {
+//   //res.send('Hello Express!');
+//   res.send({
+//     name:'Amit',
+//     interests: [
+//       'Cricket', 'Tennis'
+//     ]
+//   });
+// });
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+      title:'HBS Home Page',
+      year: new Date().getFullYear(),
+      welcomeMessage:'Welcome to About Home'
+    })
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About Page</h1>');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+      errorMessage:'Error occurred'
+    });
+});
+
+app.get('/hbsabout', (req, res) => {
+  res.render('about.hbs', {
+    title:'HBS Title Page',
+    year: new Date().getFullYear()
+  });
+});
+
+app.listen(3001);
