917cef73ab7711c0b8c8d5a630509dac1466748a
garga9 <garga9@bp.com>
2017-10-06 17:14:16 +0100

    Added comments and updated package.json

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+//Load the required modules
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+//Heroku sets the port on environment variable so accessing it from env.
+//For local run assigning the port as 3001
 const port = process.env.PORT || 3001;
 
+//Creates an Express application
 var app = express();
 
+//This line registers any files under partials directory with handlebars
+//Any file registered here can be directly used anywhere in the hbs templates
 hbs.registerPartials(__dirname + '/views/partials');
+
+//Sets the view engine as handlebar
 app.set('view engine', 'hbs');
+
+//Serve static content from public directory
 app.use(express.static(__dirname+'/public'));
 
+//Mounting a middleware function that will be executed for every request.
 app.use((req, res, next) => {
   var now = new Date().toString();
   fs.appendFile('server.log', `\n${now}: ${req.method} ${req.url}`, (err) => {
@@ -18,7 +29,7 @@ app.use((req, res, next) => {
     }
   });
   console.log(`${now}: ${req.method} ${req.url}`);
-  next();
+  next(); //This call is very important, without this call app will not proceed beyond MW
 })
 
 // app.use((req, res, next) => {
@@ -34,6 +45,7 @@ app.use((req, res, next) => {
 //   });
 // });
 
+//Defining a default route which will load the home hbs template.
 app.get('/', (req, res) => {
     res.render('home.hbs', {
       title:'HBS Home Page',
@@ -42,10 +54,12 @@ app.get('/', (req, res) => {
     })
 });
 
+//Defining an about route which will show a basic html page.
 app.get('/about', (req, res) => {
   res.send('<h1>About Page</h1>');
 });
 
+//Defining a error route which will return JSON response
 app.get('/bad', (req, res) => {
     res.send({
       errorMessage:'Error occurred'
@@ -59,6 +73,7 @@ app.get('/hbsabout', (req, res) => {
   });
 });
 
+//Starting app
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
